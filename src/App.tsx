8d8fd405a96914ce9331a663aff8556f01111697
IVAN <ivanbatur5g@gmail.com>
2021-05-10 11:28:44 +0200

    Need to finish navbar

--- src/App.tsx
+++ src/App.tsx
@@ -18,36 +18,38 @@ import OrderList from "./pages/OrderList";
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        <Route path="/order/:id" component={Order} />
-        <Route path="/shipping" component={Shipping} />
-        <Route path="/profile" component={Profile} />
-        <Route path="product/:id" component={Product} />
-        <Route path="/cart/:id?" component={Cart} />
-        <Route path="/admin/userlist" component={UserList} />
-        <Route path="/admin/user/:id/edit" component={UserEdit} />
-        <Route path="/admin/productlist" component={ProductList} />
-        <Route
-          path="/admin/productlist/:pageNumber"
-          component={ProductList}
-          exact
-        />
-        <Route path="/admin/product/:id/edit" component={ProductEdit} />
-        <Route path="/admin/orderlist" component={OrderList} />
-        <Route path="/search/:keyword" component={Home} exact />
-        <Route path="/page/:pageNumber" component={Home} exact />
-        <Route
-          path="/search/:keyword/page/:pageNumber"
-          component={Home}
-          exact
-        />
-        <Route path="/" component={Home} exact />
-      </Switch>
-      <Footer />
+      <div className="container m-auto">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/order/:id" component={Order} />
+          <Route path="/shipping" component={Shipping} />
+          <Route path="/profile" component={Profile} />
+          <Route path="product/:id" component={Product} />
+          <Route path="/cart/:id?" component={Cart} />
+          <Route path="/admin/userlist" component={UserList} />
+          <Route path="/admin/user/:id/edit" component={UserEdit} />
+          <Route path="/admin/productlist" component={ProductList} />
+          <Route
+            path="/admin/productlist/:pageNumber"
+            component={ProductList}
+            exact
+          />
+          <Route path="/admin/product/:id/edit" component={ProductEdit} />
+          <Route path="/admin/orderlist" component={OrderList} />
+          <Route path="/search/:keyword" component={Home} exact />
+          <Route path="/page/:pageNumber" component={Home} exact />
+          <Route
+            path="/search/:keyword/page/:pageNumber"
+            component={Home}
+            exact
+          />
+          <Route path="/" component={Home} exact />
+        </Switch>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 };
